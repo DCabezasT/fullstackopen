8d332440554bed04f4ce8c90e0b1ad3d9eb26d3f
DavidCabezas <129871289+DCabezasT@users.noreply.github.com>
2023-12-10 22:21:00 -0300

    Ejercicio1.3

--- part1/Ejercicio1.3/src/App.jsx
+++ part1/Ejercicio1.3/src/App.jsx
@@ -2,8 +2,6 @@ import './App.css'
 
 import React from 'react';
 
-
-
 const Part = ({ part, exercises }) => {
   return (
     <p>
@@ -29,23 +27,33 @@ const Total = ({ exercises1, exercises2, exercises3 }) => {
   );
 };
 
+
 const App = () => {
   const course = 'Half Stack application development';
   const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
   ];
-
-  const exercises1 = parts[0].exercises;
-  const exercises2 = parts[1].exercises;
-  const exercises3 = parts[2].exercises;
 
   return (
     <div>
       <h1>{course}</h1>
-      <Content parts={parts} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name}: {part.exercises} exercises
+        </p>
+      ))}
+      <p>Total exercises: {parts.reduce((total, part) => total + part.exercises, 0)}</p>
     </div>
   );
 };
