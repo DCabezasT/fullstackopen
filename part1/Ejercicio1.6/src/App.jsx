937fdcd49de3248fe935b4d9628e8c98c2afd170
DavidCabezas <129871289+DCabezasT@users.noreply.github.com>
2023-12-11 23:11:03 -0300

    Ejercicio1.7 Unicafe

--- part1/Ejercicio1.6/src/App.jsx
+++ part1/Ejercicio1.6/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 
 
-import { useState } from 'react'
-
-
+import React, { useState } from 'react';
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
@@ -11,36 +9,67 @@ const Button = ({ handleClick, text }) => (
   </button>
 );
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, allComents, average,Positive }) => {
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {allComents}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {Positive}</p>
     </div>
   );
 };
 
 const App = () => {
-  const [allClicks, setAll] = useState([])
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([]);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allComents, setAllComents] = useState(0);
+  const [totalScore, setTotalScore] = useState(0);
+  const [Positice, setPositive] = useState(0);
+
 
   const handleGoodClick = () => {
-    setAll(allClicks.concat('Good'))
-    setGood(good + 1)
-  }
+    setAll(allClicks.concat('Good'));
+    setGood(good + 1);
+    updateAllComents();
+  };
 
   const handleNeutralClick = () => {
-    setAll(allClicks.concat('Neutral'))
-    setNeutral(neutral + 1)
-  }
+    setAll(allClicks.concat('Neutral'));
+    setNeutral(neutral + 1);
+    updateAllComents();
+  };
 
   const handleBadClick = () => {
-    setAll(allClicks.concat('Bad'))
-    setBad(bad + 1)
-  }
+    setAll(allClicks.concat('Bad'));
+    setBad(bad + 1);
+    updateAllComents();
+  };
+
+  const updateAllComents = () => {
+    const total = good - bad;
+    setTotalScore(total);
+    setAllComents(good + neutral + bad + 1);
+  };
+
+  const calculateAverage = () => {
+    if (allComents === 0) {
+      return 0;
+    }
+    return totalScore / allComents;
+  };
+
+  const updatePositive = () => {
+    if (allComents === 0) {
+      return 0;
+    }
+    const positivePercentage = (good / allComents) * 100;
+    return positivePercentage.toFixed(2);
+  };
 
   return (
     <div>
@@ -51,9 +80,16 @@ const App = () => {
       <br />
       <br />
       <h1>Statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        allComents={allComents}
+        average={calculateAverage().toFixed(2)}
+        Positive={updatePositive()}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
